e783e65a655a2ac9e1ef8af20fec4b337085acad
Roman <bardlesswk@gmail.com>
2020-03-04 23:30:12 +0200

    EntityLastest

--- HWManyArchEntityLast/DAL/CarDetailsContext.cs
+++ HWManyArchEntityLast/DAL/CarDetailsContext.cs
@@ -18,42 +18,5 @@ namespace DAL
         public DbSet<Details> Details { get; set; }
         public DbSet<Manufacturers> Manufacturers { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Cars>()
-                .ToTable("Cars")
-                .HasKey(x => x.Id)
-                .Property(x => x.Name)
-                .HasMaxLength(20);
-
-            modelBuilder.Entity<Cars>()
-                .HasMany(x => x.Details)
-                .WithRequired(x => x.Car)
-                .HasForeignKey(x => x.CarsId);
-
-            modelBuilder.Entity<Details>()
-                .ToTable("Details")
-                .HasKey(x => x.Id)
-                .Property(x => x.NameDetail)
-                .HasMaxLength(20);
-
-            modelBuilder.Entity<Manufacturers>()
-                .ToTable("Manufacturers")
-                .HasKey(x => x.Id)
-                .Property(x => x.Name)
-                .HasMaxLength(20);
-
-            modelBuilder.Entity<Manufacturers>()
-                .HasMany(x => x.Cars)
-                .WithRequired(x => x.Manufacturer)
-                .HasForeignKey(x => x.ManufacturerId);
-
-            modelBuilder.Entity<Manufacturers>()
-                .HasMany(x => x.Details)
-                .WithRequired(x => x.Manufacturer)
-                .HasForeignKey(x => x.ManufacturerId);
-        }
     }
 }
---
+++ HWManyArchEntityLast/DAL/Migrations/202003042125565_NewModel_CarManfacturer.cs
@@ -0,0 +1,22 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewModel_CarManfacturer : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Cars", "Name", c => c.String());
+            AlterColumn("dbo.Details", "NameDetail", c => c.String());
+            AlterColumn("dbo.Manufacturers", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Manufacturers", "Name", c => c.String(maxLength: 20));
+            AlterColumn("dbo.Details", "NameDetail", c => c.String(maxLength: 20));
+            AlterColumn("dbo.Cars", "Name", c => c.String(maxLength: 20));
+        }
+    }
+}
